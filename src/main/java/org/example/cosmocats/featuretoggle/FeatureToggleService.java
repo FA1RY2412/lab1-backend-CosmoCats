8650de5e04556b0aaa1811bf6654b98fbf137146
FA1RY2412 <denisgolub2412@gmail.com>
2025-12-02 22:43:22 +0200

    Update FeatureToggleService.java

--- src/main/java/org/example/cosmocats/featuretoggle/FeatureToggleService.java
+++ src/main/java/org/example/cosmocats/featuretoggle/FeatureToggleService.java
@@ -3,26 +3,22 @@ package org.example.cosmocats.featuretoggle;
 import org.example.cosmocats.config.FeatureToggleProperties;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
 
 @Service
 public class FeatureToggleService {
 
-  private final ConcurrentHashMap<String, Boolean> featureToggleMap;
+    private final Map<String, Boolean> featureToggleMap;
 
-  public FeatureToggleService(FeatureToggleProperties featureToggleProperties) {
-    featureToggleMap = new ConcurrentHashMap<>(featureToggleProperties.getToggles());
-  }
+    public FeatureToggleService(FeatureToggleProperties featureToggleProperties) {
+        this.featureToggleMap = Map.copyOf(featureToggleProperties.getToggles());
+    }
 
-  public boolean checkFeatureToggle(String featureName) {
-    return featureToggleMap.getOrDefault(featureName, false);
-  }
+    public boolean checkFeatureToggle(String featureName) {
+        return featureToggleMap.getOrDefault(featureName, false);
+    }
 
-  public void enableFeatureToggle(String featureName) {
-    featureToggleMap.put(featureName, true);
-  }
-
-  public void disableFeatureToggle(String featureName) {
-    featureToggleMap.put(featureName, false);
-  }
+    public boolean checkFeatureToggle(FeatureToggles toggle) {
+        return checkFeatureToggle(toggle.getFeatureName());
+    }
 }
