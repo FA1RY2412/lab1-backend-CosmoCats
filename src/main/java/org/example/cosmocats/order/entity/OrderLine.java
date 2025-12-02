813d0aaa072501b33feee20421f8951225dba083
FA1RY2412 <denisgolub2412@gmail.com>
2025-12-02 23:35:27 +0200

    Create OrderLine.java

---
+++ src/main/java/org/example/cosmocats/order/entity/OrderLine.java
@@ -0,0 +1,51 @@
+package org.example.cosmocats.order.entity;
+
+import jakarta.persistence.*;
+import org.example.cosmocats.product.entity.Product;
+
+@Entity
+@Table(name = "order_lines")
+public class OrderLine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
