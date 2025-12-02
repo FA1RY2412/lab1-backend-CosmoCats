d345d457f37657ade3f2b22c9c1f3b597228b94b
FA1RY2412 <denisgolub2412@gmail.com>
2025-12-03 00:37:32 +0200

    Update ProductControllerIT.java

--- src/test/java/org/example/cosmocats/product/controller/ProductControllerIT.java
+++ src/test/java/org/example/cosmocats/product/controller/ProductControllerIT.java
@@ -27,6 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(PostgresTestConfig.class)
