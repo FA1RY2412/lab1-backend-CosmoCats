4a9ea294c0c0c9a8261ea70d891dae190fac4f6f
FA1RY2412 <denisgolub2412@gmail.com>
2025-12-02 21:32:38 +0200

    Update FeatureToggleProperties.java

--- src/main/java/org/example/cosmocats/config/FeatureToggleProperties.java
+++ src/main/java/org/example/cosmocats/config/FeatureToggleProperties.java
@@ -3,20 +3,19 @@ package org.example.cosmocats.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Component
 @ConfigurationProperties(prefix = "feature-toggle")
 public class FeatureToggleProperties {
 
-    private Map<String, Boolean> toggles = Collections.emptyMap();
+    private final Map<String, Boolean> toggles;
+
+    public FeatureToggleProperties(Map<String, Boolean> toggles) {
+        this.toggles = toggles;
+    }
 
     public Map<String, Boolean> getToggles() {
         return toggles;
-    }
-
-    public void setToggles(Map<String, Boolean> toggles) {
-        this.toggles = toggles;
     }
 }
