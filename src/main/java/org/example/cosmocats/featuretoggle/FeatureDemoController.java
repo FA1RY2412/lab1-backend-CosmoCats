6001ff761f95c96ff4c61af77d671145fa0c9803
FA1RY2412 <denisgolub2412@gmail.com>
2025-12-02 21:35:15 +0200

    Update FeatureDemoController.java

--- src/main/java/org/example/cosmocats/featuretoggle/FeatureDemoController.java
+++ src/main/java/org/example/cosmocats/featuretoggle/FeatureDemoController.java
@@ -1,22 +1,23 @@
 package org.example.cosmocats.featuretoggle;
 
 import org.example.cosmocats.featuretoggle.annotation.FeatureToggle;
-import org.example.cosmocats.featuretoggle.FeatureToggles;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/v1/demo")
 public class FeatureDemoController {
 
-    @GetMapping("/demo/cosmo")
+    @GetMapping("/cosmo")
     @FeatureToggle(FeatureToggles.COSMO_CATS)
     public ResponseEntity<?> demoCosmo(@RequestParam(defaultValue = "3") int count) {
         return ResponseEntity.ok(Map.of(
-            "msg", "Here are " + count + " cosmo cats"
+                "msg", "Here are " + count + " cosmo cats"
         ));
     }
 }
