dbf0ef498f4ef15b20d774c0728cdb7fc3156ff1
FA1RY2412 <denisgolub2412@gmail.com>
2025-12-03 00:38:23 +0200

    Update ProductControllerIT.java

--- src/test/java/org/example/cosmocats/product/controller/ProductControllerIT.java
+++ src/test/java/org/example/cosmocats/product/controller/ProductControllerIT.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.ActiveProfiles;
 
 import jakarta.transaction.Transactional;
 
