27398bd5ae3b974eda9ea7409f94bb6409df4552
FA1RY2412 <denisgolub2412@gmail.com>
2025-12-06 00:46:53 +0200

    Update OrderRepository.java

--- src/main/java/org/example/cosmocats/order/repository/OrderRepository.java
+++ src/main/java/org/example/cosmocats/order/repository/OrderRepository.java
@@ -1,10 +1,10 @@
 package org.example.cosmocats.order.repository;
 
-import org.example.cosmocats.order.entity.Order;
+import org.example.cosmocats.order.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    Optional<Order> findByNumber(String number);
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    Optional<OrderEntity> findByNumber(String number);
 }
